1f210c31afaf316737db4658eba34c47f84f3353
seo02wow <rlatjdud9188@naver.com>
2024-06-05 15:36:25 +0900

    :lipstick:design: 드롭다운 반응형 적용

--- pages/boards.tsx
+++ pages/boards.tsx
@@ -49,9 +49,9 @@ export default function Board() {
   useEffect(() => {
     const handleWindowResize = () => {
       const windowWidth = window.innerWidth;
-      if (windowWidth <= 768) {
+      if (windowWidth <= 767) {
         setBestPageSize(1);
-      } else if (windowWidth <= 1200) {
+      } else if (windowWidth <= 1199) {
         setBestPageSize(2);
       } else {
         setBestPageSize(3); // 기본값 3으로 설정
